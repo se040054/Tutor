49f6528e2797e0c25cf31b94a53b8d5a2b7e73ca
se040054 <peter8822967@gmail.com>
2023-09-15 00:02:45 +0800

    more authentication in addLession api

--- src/services/teacher-service.js
+++ src/services/teacher-service.js
@@ -4,8 +4,20 @@ require('moment-timezone').tz.setDefault('Asia/Taipei')
 
 const teacherService = {
   addLesson: (req, next) => {
-    const { duration, daytime } = req.body
     const teacherId = req.user.Teacher.id
+    const { duration, daytime } = req.body
+    const MAXIMUM_DURATION_MINUTE = 480
+    const MINIMUM_DURATION_MINUTE = 30
+    const EARLIEST_DAYTIME_HOUR = 18
+    const LATEST_DAYTIME_HOUR = 22
+    const now = moment()
+    const createdLessonStart = moment(daytime)
+    const createdLessonEnd = createdLessonStart.clone().add(duration, 'minutes')
+    if (duration < MINIMUM_DURATION_MINUTE ||
+      duration > MAXIMUM_DURATION_MINUTE) throw new Error('課程時長不符合')
+    if (createdLessonStart.hour() < EARLIEST_DAYTIME_HOUR ||
+      createdLessonEnd.hour() >= LATEST_DAYTIME_HOUR) throw new Error('上課時段不符合')
+    if (createdLessonStart.isBefore(now)) throw new Error('創建的課程時間已過')
     return Teacher.findByPk(teacherId, {
       include: [Lesson],
       nest: true
@@ -14,24 +26,22 @@ const teacherService = {
         // console.log(`老師的課程 : ${JSON.stringify(teacher.Lessons)}`)
         // 開始時間:課程時間，結束時間:課程時間+時長，開始時間與結束時間丟進新創建的時間比對
         // 注意moment.add因為是對物件做，調用會變成淺拷貝 要用clone
-        const createdLessonStart = moment(daytime)
-        const createdLessonEnd = createdLessonStart.clone().add(duration, 'minutes')
         for (let i = 0; i < teacher.Lessons.length; i++) {
           const startTime = moment(teacher.Lessons[i].daytime)
           const endTime = startTime.clone().add(teacher.Lessons[i].duration, 'minutes')
           if (startTime.isSameOrBefore(createdLessonEnd) &&
             endTime.isSameOrAfter(createdLessonStart)) {
-          //  console.log('有重複的錯誤偵測')
+            //  console.log('有重複的錯誤偵測')
             throw new Error('課程時間重複')
           }
-          // console.log(typeof duration, typeof teacher.Lessons[i].duration)
           // console.log(`比對結果 :新課時間${createdLessonStart}~${createdLessonEnd}`)
           // console.log(`比對結果 :舊課時間${startTime}~${endTime}`)
         }
         return Lesson.create({
           daytime,
           duration,
-          teacherId
+          teacherId,
+          isReserved: false
         })
       }).then(createdLesson => {
         return next(null, {
