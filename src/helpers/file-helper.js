68c3461fd613fa618b75149d67f219a655db3c19
se040054 <peter8822967@gmail.com>
2023-09-24 01:28:47 +0800

    try

--- src/helpers/file-helper.js
+++ src/helpers/file-helper.js
@@ -2,6 +2,9 @@ const fs = require('fs') // 引入 fs 模組
 const localFileHandler = file => { // file 是 multer 處理完的檔案
   return new Promise((resolve, reject) => {
     if (!file) return resolve(null) // 如果Multer沒傳 就終止
+    // if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+    //   return resolve(new Error('僅接受jpg jpeg png'))
+    // }
     const fileName = `public/images/${file.originalname}`
     return fs.promises.readFile(file.path)
       .then(data => fs.promises.writeFile(fileName, data))
