c87518f67798e9073954df92846a79cec542d76e
se040054 <peter8822967@gmail.com>
2023-09-24 23:20:36 +0800

    modify : admin get users can seach and page

--- src/services/admin-service.js
+++ src/services/admin-service.js
@@ -1,11 +1,29 @@
+const { Op } = require('sequelize')
 const { User } = require('../db/models')
 const adminService = {
-  getUsers: (req, next) => {
-    return User.findAll()
+  getUsers: async (req, next) => {
+    let currentPage = req.query.page || 1
+    const { search } = req.query || null
+    const USER_PER_PAGE = 7
+
+    const amount = await User.count({
+      where: search ? { name: { [Op.substring]: `${search}` } } : null
+    })
+
+    const totalPage = Math.ceil(amount / USER_PER_PAGE)
+    if (currentPage > totalPage) currentPage = totalPage
+    if (currentPage < 1) currentPage = 1
+    const offset = (currentPage - 1) * USER_PER_PAGE
+    return User.findAll({
+      where: search ? { name: { [Op.substring]: `${search}` } } : null,
+      limit: USER_PER_PAGE,
+      offset
+    })
       .then(users => {
         return next(null, {
           status: 'success',
-          users
+          users,
+          totalAmount: amount
         })
       })
   }
