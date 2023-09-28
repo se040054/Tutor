362001240f6e77a3af3312e210728cec3dfe931a
se040054 <peter8822967@gmail.com>
2023-09-29 00:57:45 +0800

    test production

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-require('dotenv').config({ path: './environment/dev/.env' })
-const port = process.env.PORT
+
+if (process.env.NODE_ENV === 'production') {
+  require('dotenv').config({ path: './environment/prod/.env' })
+}
+
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: './environment/dev/.env' })
+}
+
 const express = require('express')
 const app = express()
+
 const cors = require('cors')
-app.use(cors())
+app.use(cors()) // 測試
 
 const apis = require('./src/routes/index')
 
@@ -13,12 +21,13 @@ app.use(express.json())
 
 app.use('/api', apis)
 
-app.get('/', (req, res) => {
-  res.send('dev/dev_home')
+app.get('/', (req, res) => { // 測試
+  res.send('dev_home')
 })
 
-app.listen(port, () => {
-  console.info(`http://localhost:${port}`)
-  console.log(`for api http://localhost:${port}/api `)
-  console.log(`for auth http://localhost:${port}/api/auth/google `)
+app.listen(process.env.PORT, () => {
+  console.log(`目前為:${process.env.NODE_ENV} 環境`)
+  console.info(`http://localhost:${process.env.PORT}`)
+  // console.log(`for api http://localhost:${process.env.PORT}/api `)
+  // console.log(`for auth http://localhost:${process.env.PORT}/api/auth/google `)
 })
